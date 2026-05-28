55f3082afce13adde9dc6d79f163455101533475
Juwayriya6 <juwayriyaart@gmail.com>
2026-05-28 10:43:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const loader = document.querySelector("#loader");
+lconst loader = document.querySelector("#loader");
 
 window.addEventListener("load", () => {
   setTimeout(() => loader?.classList.add("hide"), 450);
